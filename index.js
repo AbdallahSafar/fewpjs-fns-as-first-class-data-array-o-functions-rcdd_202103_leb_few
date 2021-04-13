ed0f9fe4a4bdf18181b0ffcf2c8c3c258e73472a
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-13 19:42:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function wakeDog(dogName,dogBreed)
   console.log(str);
   return str;
 }
+
+function leashDog(dogName,dogBreed)
+{
+  const str = `Leash ${dogName} the ${dogBreed}`;
+  console.log(str);
+  return str;
+}
